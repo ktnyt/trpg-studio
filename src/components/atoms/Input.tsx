ac86b2548605c588da542259b3d7eaa23d482352
Kotone Itaya <kotone@sfc.keio.ac.jp>
2021-01-27 19:12:00 +0900

    Remove unnecessary memoization

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -2,7 +2,6 @@ import {
   ChangeEvent,
   ComponentPropsWithRef,
   forwardRef,
-  memo,
   useEffect,
   useRef,
   useState,
@@ -18,56 +17,54 @@ export type InputProps = ComponentPropsWithRef<'input'> & {
 type EventState = ChangeEvent<HTMLInputElement> | null
 
 export const Input = Object.assign(
-  memo(
-    forwardRef<HTMLInputElement, InputProps>(
-      ({ onKeyDown, onChange, debounce = 0, ...props }, ref) => {
-        const localRef = useRef<HTMLInputElement>(null!)
+  forwardRef<HTMLInputElement, InputProps>(
+    ({ onKeyDown, onChange, debounce = 0, ...props }, ref) => {
+      const localRef = useRef<HTMLInputElement>(null!)
 
-        const [composing, setComposing] = useState(false)
-        const [event, setEvent] = useState<EventState>(null)
+      const [composing, setComposing] = useState(false)
+      const [event, setEvent] = useState<EventState>(null)
 
-        const debounced = useDebounce(event, debounce)
-        const different = useDifferent(debounced)
+      const debounced = useDebounce(event, debounce)
+      const different = useDifferent(debounced)
 
-        useEffect(() => {
-          if (
-            !composing &&
-            onChange !== undefined &&
-            debounced !== null &&
-            different
-          ) {
-            onChange(debounced)
-          }
-        }, [composing, onChange, debounced, different])
+      useEffect(() => {
+        if (
+          !composing &&
+          onChange !== undefined &&
+          debounced !== null &&
+          different
+        ) {
+          onChange(debounced)
+        }
+      }, [composing, onChange, debounced, different])
 
-        return (
-          <input
-            ref={(current) => {
-              if (ref instanceof Function) {
-                ref(current)
-              } else if (ref !== null) {
-                ref.current = current
-              }
-              if (current !== null) {
-                localRef.current = current
-              }
-            }}
-            onKeyDown={(event) => {
-              if (onKeyDown !== undefined) {
-                onKeyDown(event)
-              }
-              if (event.key === 'Escape') {
-                localRef.current.blur()
-              }
-            }}
-            onCompositionStart={() => setComposing(true)}
-            onCompositionEnd={() => setComposing(false)}
-            onChange={setEvent}
-            {...props}
-          />
-        )
-      }
-    )
+      return (
+        <input
+          ref={(current) => {
+            if (ref instanceof Function) {
+              ref(current)
+            } else if (ref !== null) {
+              ref.current = current
+            }
+            if (current !== null) {
+              localRef.current = current
+            }
+          }}
+          onKeyDown={(event) => {
+            if (onKeyDown !== undefined) {
+              onKeyDown(event)
+            }
+            if (event.key === 'Escape') {
+              localRef.current.blur()
+            }
+          }}
+          onCompositionStart={() => setComposing(true)}
+          onCompositionEnd={() => setComposing(false)}
+          onChange={setEvent}
+          {...props}
+        />
+      )
+    }
   ),
   { displayName: 'Input' }
 )
