5b624ba4f4f6dace1d13bf2477f883ea21b70cf8
Kotone Itaya <kotone@sfc.keio.ac.jp>
2021-02-01 09:08:29 +0900

    Update useReferrer

--- src/hooks/useReferrer.tsx
+++ src/hooks/useReferrer.tsx
@@ -1,5 +1,5 @@
+import { useRef } from 'react'
+
 import { nanoid } from 'nanoid'
 
-const referrer = nanoid()
-
-export const useReferrer = () => referrer
+export const useReferrer = () => useRef(nanoid()).current
