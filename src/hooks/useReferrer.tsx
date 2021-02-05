645884f3d4835e89197185c564a967bd9b638cfb
Kotone Itaya <kotone@sfc.keio.ac.jp>
2021-02-05 10:05:30 +0900

    Modify referrer to persist on browser

--- src/hooks/useReferrer.tsx
+++ src/hooks/useReferrer.tsx
@@ -1,5 +1,14 @@
-import { useRef } from 'react'
+import { useEffect } from 'react'
 
 import { nanoid } from 'nanoid'
 
-export const useReferrer = () => useRef(nanoid()).current
+export const useReferrer = () => {
+  const saved = window.localStorage.getItem('referrer')
+  const referrer = saved === null ? nanoid() : saved
+  useEffect(() => {
+    if (saved === null) {
+      window.localStorage.setItem('referrer', referrer)
+    }
+  })
+  return referrer
+}
